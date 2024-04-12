55437eb1005f004d02a7a37d0bb6c3fc3bc7bcea
micx83 <166127123+micx83@users.noreply.github.com>
2024-04-12 14:50:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 document.getElementById("render-button").addEventListener("click", function() {
   var text = document.getElementById("text-input").value;
   var font = document.getElementById("font-selector").value;
-  var text3D = document.getElementById("text3D");
-  text3D.setAttribute('value', text);
-  text3D.setAttribute('color', '#C0C0C0'); // Silver color
-  text3D.setAttribute('font', font);
-  text3D.setAttribute('height', '7'); // Maximum height of 7
+  var outputText = document.getElementById("output-text");
+  outputText.textContent = text;
+  outputText.style.fontFamily = font;
 });
